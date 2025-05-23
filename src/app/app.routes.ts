a56c8b2f108f4120bb0aae81d37a3a041319ef00
jasontran103 <jasontran103@gmail.com>
2025-05-22 17:55:40 -0700

    feat: Added app routing for the home and product pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { ProductListComponent } from './features/products/components/product-list/product-list.component';
+import { ProductDetailComponent } from './features/products/components/product-detail/product-detail.component';
+import { HomeComponent } from './features/home/components/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'products', component: ProductListComponent },
+  // This route will be used to display the product detail page depending on the id
+  { path: 'products/:id', component: ProductDetailComponent },
+  // This route will be used to redirect to the product list page if the route is not found
+  { path: '**', redirectTo: '/products' }
+];
