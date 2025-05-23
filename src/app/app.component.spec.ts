f0436de6723d573311803826cc0ea0d0fc441565
jasontran103 <jasontran103@gmail.com>
2025-05-22 17:56:34 -0700

    feat: Added the header to the root component of the application

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,9 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
+/**
+ * Test suite for the AppComponent.
+ */
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -8,18 +11,27 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
+  /**
+   * Test to check if the app component is created.
+   */
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
+  /**
+   * Test to check if the app component has the 'product-app' title.
+   */
   it(`should have the 'product-app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('product-app');
   });
 
+  /**
+   * Test to check if the app component has the 'Hello, product-app' title.
+   */
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
